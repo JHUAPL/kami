573b75dc271ebad349fe8f0929e57e58e0271443
James P. Howard, II <james.howard@jhuapl.edu>
2020-12-14 15:13:16 -0500

    Added license information

--- examples/boltzmann1d/boltzmann1d.cc
+++ examples/boltzmann1d/boltzmann1d.cc
@@ -1,9 +1,35 @@
 /*-
- * TODO FILEHEADER
+ * Copyright (c) 2020 The Johns Hopkins University Applied Physics
+ * Laboratory LLC
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include "boltzmann1d.h"
 
+#include <kami/agent.h>
+#include <kami/config.h>
+#include <kami/grid2d.h>
+#include <kami/kami.h>
+#include <kami/multigrid2d.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 #include <spdlog/stopwatch.h>
@@ -13,11 +39,6 @@
 #include <CLI/Config.hpp>
 #include <CLI/Formatter.hpp>
 #include <iostream>
-#include <kami/agent.h>
-#include <kami/config.h>
-#include <kami/grid2d.h>
-#include <kami/kami.h>
-#include <kami/multigrid2d.h>
 #include <map>
 #include <string>
 
