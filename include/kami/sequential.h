2735cef23e1d67b0c3420685e956b963688dd685
James P. Howard, II <james.howard@jhuapl.edu>
2021-07-15 13:13:15 -0400

    Standardize the documentation format

--- include/kami/sequential.h
+++ include/kami/sequential.h
@@ -37,36 +37,52 @@
 
 namespace kami {
 
-///  \brief     Will execute all agent steps in a sequential order.
-///  \details   A sequential scheduler will iterate over the agents assigned
-///  to the scheduler and call their `step()` function in a sequential order.
-///  That order is preserved between calls to `step()` but may be modified by
-///  `addAgent()` or `deleteAgent()`.
-///  \pre       First create a Model for the scheduler to live in.
+/**
+ * @brief Will execute all agent steps in a sequential order.
+ *
+ * @details A sequential scheduler will iterate over the agents assigned
+ * to the scheduler and call their `step()` function in a sequential order.
+ * That order is preserved between calls to `step()` but may be modified by
+ * `addAgent()` or `deleteAgent()`.
+ *
+ * \pre       First create a Model for the scheduler to live in.
+ */
 class LIBKAMI_EXPORT SequentialScheduler : public Scheduler {
    public:
-    ///  \brief Constructor.
-    ///  \details   The Model parameter is used by the scheduler to get
-    ///  access to an Agent.  The Model is presumed to maintain a master
-    ///  list of all Agents in the Model and the Model can be queried for
-    ///  a reference to any particular Agent at `step()` time.
+    /**
+     * @brief Constructor.
+     *
+     * @details The Model parameter is used by the scheduler to get
+     * access to an Agent.  The Model is presumed to maintain a master
+     * list of all Agents in the Model and the Model can be queried for
+     * a reference to any particular Agent at `step()` time.
+     */
     SequentialScheduler(Model *model);
 
-    ///  \brief Add an agent to the scheduler.
-    ///  \details   The scheduler maintains a list of all AgentIDs currently
-    ///  assigned.  This function adds a new Agent to the list.
+    /**
+     * @brief Add an agent to the scheduler.
+     *
+     * @details The scheduler maintains a list of all AgentIDs currently
+     * assigned.  This function adds a new Agent to the list.
+     */
     void add_agent(AgentID agent_id);
 
-    ///  \brief Remove an agent from the scheduler.
-    ///  \details The scheduler maintains a list of all AgentIDs currently
-    ///  assigned.  This function removes an Agent from the list.
+    /**
+     * @brief Remove an agent from the scheduler.
+     *
+     * @details The scheduler maintains a list of all AgentIDs currently
+     * assigned.  This function removes an Agent from the list.
+     */
     void delete_agent(AgentID agent_id);
 
-    ///  \brief Execute a single time step.
-    ///  \details   This method will step through the list of Agents in the
-    ///  scheduler's internal queue and then execute the `Agent::step()`
-    ///  method for every Agent assigned to this scheduler in the order
-    ///  assigned.
+    /**
+     * @brief Execute a single time step.
+     *
+     * @details This method will step through the list of Agents in the
+     * scheduler's internal queue and then execute the `Agent::step()`
+     * method for every Agent assigned to this scheduler in the order
+     * assigned.
+     */
     void step();
 
    protected:
@@ -76,7 +92,7 @@ class LIBKAMI_EXPORT SequentialScheduler : public Scheduler {
      */
     std::vector<AgentID> _agent_list;
 
-    /** 
+    /**
      * A pointer to the `Model` this scehduler belongs to
      */
     Model *_model;
